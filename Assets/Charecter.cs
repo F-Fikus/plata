3063bffcfcbc93fc86c2aa56ffe6580ad6c3d07e
F-Fikus <fikus.f.60@mail.ru>
2020-08-12 17:09:40 +0300

    Init

---
+++ Assets/Charecter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charecter : MonoBehaviour
+{
+    public float speedMove;
+    public float jumpPower;
+    private float gravityForce;
+    private Vector3 moveVector;
+    private CharacterController ch_controller;
+    private MobileController mContr;
+    void Start()
+    {
+        ch_controller = GetComponent<CharacterController>();
+        mContr = GameObject.FindGameObjectWithTag("Joystick").GetComponent<MobileController>();
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        CharacterMove();
+        GamingGravity();
+    }
+    private void CharacterMove()
+    {
+        moveVector = Vector3.zero;
+        moveVector.x = mContr.Horizontal() * speedMove;
+        moveVector.z = mContr.Vertical() * speedMove;
+
+        
+
+        
+        if (Vector3.Angle(Vector3.forward, moveVector) > 1f || Vector3.Angle(Vector3.forward, moveVector) == 0)
+        {
+            Vector3 direct = Vector3.RotateTowards(transform.forward, moveVector, speedMove, 0.0f);
+            transform.rotation = Quaternion.LookRotation(direct);
+        }
+        moveVector.y = gravityForce;
+        ch_controller.Move(moveVector * Time.deltaTime);
+    }
+    private void GamingGravity()
+    {
+        if (!ch_controller.isGrounded)gravityForce -= 20f * Time.deltaTime;
+        else gravityForce = -1f;
+       
+    }
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public GameObject Sphere;
+    public int S;
+
+    public void OnButtonDown()
+    {
+        S = Random.Range(1, 2);
+        Sphere.transform.Translate(S, 0, 0);
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
